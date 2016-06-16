65d81edc5bded814a75b9f0e616359f5144c1beb
wcollins <wcollins@ebsco.com>
2016-06-16 16:59:33 -0400

    removed unnecessary code

--- make.js
+++ make.js
@@ -485,11 +485,6 @@ target.bundle = function(args) {
   echo('### Bundling files into ' + BUILD_TARGET);
 
   function bundle(filename, outfilename, files, distname, isMainFile) {
-    var out = '',
-      lineFilter = function (line) {
-        out += line.replace(/['"]use strict['"];/g, "") + '\n';
-      };
-
     var bundleContent = cat(files),
         bundleVersion = VERSION,
         bundleBuild = exec('git log --format="%h" -n 1',
@@ -511,15 +506,13 @@ target.bundle = function(args) {
     });
     // This just preprocesses the empty pdf.js file, we don't actually want to
     // preprocess everything yet since other build targets use this file.
-    builder.preprocess(filename, lineFilter, builder.merge(defines,
+    builder.preprocess(filename, outfilename, builder.merge(defines,
                            {BUNDLE: bundleContent,
                             BUNDLE_VERSION: bundleVersion,
                             BUNDLE_BUILD: bundleBuild,
                             BUNDLE_AMD_NAME: amdName,
                             BUNDLE_JS_NAME: jsName,
                             MAIN_FILE: isMainFile}));
-
-    fs.writeFileSync(outfilename, out);
   }
 
   if (!test('-d', BUILD_DIR)) {
